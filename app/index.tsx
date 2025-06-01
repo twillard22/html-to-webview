b38d532b8699e1c67f4c271cfc9ce2b496ecf6ed
twillard22 <48259458+twillard22@users.noreply.github.com>
2025-06-01 13:06:58 -0500

    removing groq

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,8 @@
 import WebviewComponent from "@/components/webview-components";
-import Groq from "groq-sdk";
+
 import React from "react";
 import html from "../static/test.html";
-const groq = new Groq({ apiKey: process.env.EXPO_PUBLIC_GROQ_API_KEY });
-
-export async function getGroqChatCompletion(
-  question: string = "Explain the importance of fast language models"
-) {
-  return groq.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content: question,
-      },
-    ],
-    model: "llama-3.3-70b-versatile",
-  });
-}
 
 export default function Index() {
-  // const [answer, setAnswer] = useState("");
-
-  // useEffect(() => {
-  //   getGroqChatCompletion(
-  //     "can you generate html to show a small landing page, only return the html, no other text, and don't wrap it with any tags"
-  //   ).then((answer) => {
-  //     setAnswer(answer?.choices[0].message.content ?? "");
-  //   });
-  // }, []);
-
   return <WebviewComponent>{html}</WebviewComponent>;
 }
